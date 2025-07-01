764adcf4f51348928c9d5ad2eb0368930a70f5fd
Inyene Etoedia <etoedia@gmail.com>
2025-07-01 10:10:17 -0700

    Initial Commit

---
+++ Cowrywise-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cowrywise-Task
+//
+//  Created by Inyene on 7/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
